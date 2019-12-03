eab90e3093a561f2e7394ba9fcb2d5836ffca797
danilo <danilo.marmentini@gmail.com>
2019-12-03 01:48:38 -0300

    adição de vue no projeto e parte feita por Danilo

---
+++ src/components/Checkout.vue
@@ -0,0 +1,85 @@
+<template>
+<div>
+  <div class="divTabs">
+    <i class="fas fa-cash-register"></i><h2 class="checkoutTitle">Checkout</h2>
+    <div class="row container">
+      <div class="col s12">
+        <ul class="tabs">
+          <li class="tab col s2"><a href="#test2">Endereços</a></li>
+          <li class="tab col s2"><a href="#test1">Pagamento</a></li>
+        </ul>
+      </div>
+      <div id="test2" class="col s12 insideTab"></div>
+      <div id="test1" class="col s12 insideTab"></div>
+    </div>
+  </div>
+  <div class="resumoCheckout">
+    <div>Resumo do pedido</div>
+    <div>Itens:</div><div>qtd</div>
+    <div>Subtotal:</div><div>valor</div>
+    <div>Desconto</div><div>valor</div>
+    <div>Total</div><div>valor</div>
+</div>
+    <a class="waves-effect waves-light btn btnFinal">Finalizar</a>
+</div>
+</template>
+
+<script>
+// import axios from "axios";
+
+export default {
+  name: 'Checkout',
+  data () {
+    return {}
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+.divTabs{
+    float: left;
+    width: 60%;
+    margin-left: 10em;;
+}
+
+.cartTitle{
+    padding-left: 4em;
+}
+
+.row .insideTab{
+    padding-top: 1.5em;
+}
+
+.resumoCheckout{
+    float: left;
+    width: 25%;
+    padding-top: 7em;
+    text-align: left;
+    border: red 5px;
+}
+
+.resumoCheckout div{
+    width: 50%;
+    display: inline-block;
+    padding-top: 1.5em;
+}
+
+body{
+    margin-top: 5em;
+}
+
+.btnFinal{
+    margin-top: 3em;
+    float: right;
+    margin-right: 10%;
+}
+
+.checkoutTitle{
+    margin-left: 3.3em;
+}
+.tabs .tab{
+  width: 50%;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,164 @@
+<template>
+  <div id="header">
+    <nav class="header-content header-content--top">
+      <div>
+        <router-link to="/home" class="brand-logo">
+          <i class="material-icons">cloud</i>Logo
+        </router-link>
+      </div>
+      <div class="nav-wrapper header">
+        <form>
+          <div class="input-field">
+            <input id="search" type="search" required />
+            <label class="label-icon" for="search">
+              <i class="material-icons">search</i>
+            </label>
+            <i class="material-icons">close</i>
+          </div>
+        </form>
+      </div>
+      <div>
+        <ul class="right hide-on-med-and-down">
+          <li>
+            <!-- Dropdown Trigger -->
+            <a class="dropdown-trigger user" data-target="dropdown1">
+              <span v-if="userLoggedIn">{{userName}}</span>
+              <span v-if="!userLoggedIn">Usuario</span>
+              <i class="material-icons right">arrow_drop_down</i>
+            </a>
+          </li>
+
+          <li>
+            <router-link to="/cart">Carrninho</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <nav class="header-content header-content--bottom">
+      <ul v-if="categorys && categorys.length > 0" class="right hide-on-med-and-down">
+        <li v-for="category in categorys" :key="category.id">
+          <router-link
+            :to="{ name: 'ProductListing', params: { description: category.description }} "
+          >{{category.description}}</router-link>
+        </li>
+      </ul>
+    </nav>
+
+    <ul id="dropdown1" class="dropdown-content">
+      <li>
+        <router-link v-if="userLoggedIn" to="/myaccount">Minha Conta</router-link>
+        <router-link v-if="!userLoggedIn" to="/login">Logar</router-link>
+      </li>
+      <li>
+        <router-link v-if="!userLoggedIn" to="/register">Cadastrar</router-link>
+        <a @click="callLogout" v-if="userLoggedIn">Logout</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Header',
+  data () {
+    return {
+      categorys: [],
+      userName: this.getUserName(),
+      userLoggedIn: this.getUserLoggedIn()
+
+    }
+  },
+  mounted () {
+    axios.get('https://floating-sands-83864.herokuapp.com/category?search=')
+      // JSON responses are automatically parsed.
+      .then(response => {
+        this.categorys = response.data
+      }).catch(e => {
+        console.log(e)
+      })
+  },
+  methods: {
+    getUserName: function () {
+      var logged = localStorage.getItem('user_logged')
+      if (logged === 'true') {
+      }
+      return localStorage.getItem('user_firstName')
+    },
+
+    getUserLoggedIn: function () {
+      var logged = localStorage.getItem('user_logged')
+      return logged === 'true'
+    },
+
+    callLogout: function () {
+      var data = {
+        'success': true
+      }
+
+      axios.post('https://floating-sands-83864.herokuapp.com/logout', data)
+        // JSON responses are automatically parsed.
+        .then(response => {
+          this.userLoggedIn = false
+        }).catch(e => {
+          console.log(e)
+        })
+    }
+  },
+  watch: {
+    '$route.path': function (path) {
+      this.userName = this.getUserName()
+      this.userLoggedIn = this.getUserLoggedIn()
+    },
+    userLoggedIn: function (value) {
+      localStorage.setItem('user_logged', value)
+      console.log('okay', value)
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+#header {
+  width: 100%;
+  -webkit-box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+    0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2);
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+    0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2);
+
+  .header-content {
+    display: flex;
+    align-items: center;
+    padding: 0 20px;
+    box-shadow: none;
+
+    &--top {
+      justify-content: space-between;
+
+      .nav-wrapper.header {
+        width: 50%;
+        min-width: 400px;
+      }
+
+      .brand-logo {
+        position: relative;
+        display: flex;
+        align-items: center;
+      }
+      .dropdown-trigger {
+        &.user {
+          display: flex;
+          align-items: center;
+        }
+      }
+    }
+
+    &--bottom {
+      justify-content: center;
+    }
+  }
+}
+</style>
